22091c15d7a7c9606cd39ade266e92b6fe784282
Arnold138 <belhocine.arnold@gmail.com>
2025-04-14 16:14:17 +0200

    r

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     const email= document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    fetch("http://localhost:5678/api/users/login", { 
+    fetch('http://localhost:5678/api/users/login', { 
 
         method:"POST", 
         headers: {
