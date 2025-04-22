e4491c282d547f0ced5dd2e4a931ef7d68cedb6e
Arnold138 <belhocine.arnold@gmail.com>
2025-04-22 15:51:20 +0200

    test cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express       = require('express');
+const path          = require('path');
+const cors          = require('cors');
 require('dotenv').config();
-const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express');
-const yaml = require('yamljs');
-const swaggerDocs = yaml.load('swagger.yaml');
+const helmet        = require('helmet');
+const swaggerUi     = require('swagger-ui-express');
+const yaml          = require('yamljs');
+const swaggerDocs   = yaml.load('swagger.yaml');
+
 const app = express();
+
+// CORS : accepte toutes les origines, méthodes et headers nécessaires
 const corsOptions = {
-  origin: 'http://localhost:5500',
+  origin: true,  // renvoie l'origine de la requête (file://, 127.0.0.1:5500, etc.)
   methods: ['GET','POST','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true
@@ -16,22 +19,25 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-const db = require("./models");
-const userRoutes = require('./routes/user.routes');
-const categoriesRoutes = require('./routes/categories.routes');
-const worksRoutes = require('./routes/works.routes');
-
+// ORM / BD
+const db = require('./models');
 db.sequelize.sync().then(() => console.log('db is ready'));
 
-app.use('/api/users', userRoutes);
+// Routes
+const userRoutes       = require('./routes/user.routes');
+const categoriesRoutes = require('./routes/categories.routes');
+const worksRoutes      = require('./routes/works.routes');
+
+app.use('/api/users',      userRoutes);
 app.use('/api/categories', categoriesRoutes);
-app.use('/api/works', worksRoutes);
+app.use('/api/works',      worksRoutes);
+
+// Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = app;
