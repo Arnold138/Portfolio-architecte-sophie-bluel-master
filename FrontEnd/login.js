9767794ddb81d5a82b2a49f2d2a166bd443255ef
Arnold138 <belhocine.arnold@gmail.com>
2025-05-04 16:16:55 +0200

    Ajout d'une nouvelle erreur pour le login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,8 +1,17 @@
 document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault(); /* empeche l'envoie classique du formualire */
 
-    const email= document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const errorMsg = document.getElementById("loginError");
+  
+    const email = emailInput.value;
+    const password = passwordInput.value;
+  
+    // Réinitialiser l'état des inputs
+    emailInput.classList.remove("error");
+    passwordInput.classList.remove("error");
+    errorMsg.style.display = "none";
 
     fetch('http://localhost:5678/api/users/login', { 
 
@@ -24,7 +33,10 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
             alert("Connexion réussie !"); /* va rediriger ou affiche un message */
             window.location.href = "index.html"; /* redirection vers la page d'accueil */
         } else { 
-            alert("Identifiants incorrects."); 
+            emailInput.classList.add("error");
+            passwordInput.classList.add("error");
+            errorMsg.textContent = "Identifiant ou mot de passe incorrect";
+            errorMsg.style.display = "block";
         }
     })
     .catch(err => { 
